e07ec8f18d248da48f9d01b16e78d4fd67a7941e
zaibali <zaibali51@live.com>
2024-08-30 20:32:44 +0500

    context init

---
+++ src/app/context/studentcontext.tsx
@@ -0,0 +1,12 @@
+import { createContext } from "react";
+
+ 
+ const mainContext = createContext(null);
+
+ export default function Studentcontext({children} : {children: React.ReactDOM}) {
+  let studentData = [
+    {
+      
+    }
+  ]
+ }
