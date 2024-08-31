9726186a4acb9903829aeb9cf2cca2f17abbf51d
zaibali <zaibali51@live.com>
2024-08-31 22:24:59 +0500

    student context added

--- src/app/context/studentcontext.tsx
+++ src/app/context/studentcontext.tsx
@@ -1,12 +1,30 @@
-import { createContext } from "react";
+"use client"
+import { createContext, useState } from "react";
 
  
  const mainContext = createContext(null);
 
- export default function Studentcontext({children} : {children: React.ReactDOM}) {
+ export default function Studentcontext({children} : {children: React.ReactNode}) {
   let studentData = [
     {
-      
+      name: "Zayb Ali",
+      fatherName: "Muhammad Ismail",
+      rollNumber: 175561,
+      feeStatus: true
+    },
+    {
+      name: "Muhammad Umar",
+      fatherName: "Mehmood",
+      rollNumber: 175580,
+      feeStatus: false
     }
-  ]
+  ];
+
+  const [allStudents, setAllStudents] = useState(studentData);
+
+  return (
+    <mainContext.Provider value={{allStudents, setAllStudents}}>
+      {children}
+    </mainContext.Provider>
+  )
  }
